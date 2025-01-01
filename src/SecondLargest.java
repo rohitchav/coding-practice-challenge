9022ff2a1ad65db906c2148b4904fdf99ca0f1e8
rohitchavan <rohitchavan4007@gmail.com>
2025-01-01 23:19:11 +0530

    Make Some Change

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class SecondLargest {
+import java.util.Scanner;
+
+public class SecondLargest{
+    public static int secondLargestElement(int[] arr,int n){
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i=1;i<=n-1;i++){
+            if(arr[i]>largest){
+                secondLargest =largest;
+                largest=arr[i];
+            }else if(arr[i] < largest && secondLargest < arr[i]){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int []array = {19,16,20,10,21,21};
+        int n = array.length;
+        System.out.println(secondLargestElement(array,n));
+    }
 }
